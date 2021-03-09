78fb37b5c04ea9f532ee9dca60b97070175632f7
Kevin Pruvost <kevin.pruvost@epitech.eu>
2021-03-09 21:50:46 +0100

    work xp listed

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -23,7 +23,17 @@ class Resume extends Component {
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+            <ul>{
+                work.description.map(function(item, idx) {
+                  return (
+                    <li>
+                          {item}
+                          <br/>
+                    </li>
+                   )
+              })
+                
+            }</ul>
         </div>
       })
 
